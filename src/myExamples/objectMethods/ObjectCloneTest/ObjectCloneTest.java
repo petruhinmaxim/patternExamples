2e0b04cd23a1d36c977260c43623d19fdcff80e0
petruhinmaxim <petruhinmaxim@gmail.com>
2017-05-13 23:19:11 +0300

    clone method object

--- src/myExamples/objectMethods/ObjectCloneTest/ObjectCloneTest.java
+++ src/myExamples/objectMethods/ObjectCloneTest/ObjectCloneTest.java
@@ -1,6 +1,7 @@
 package myExamples.objectMethods.ObjectCloneTest;
 
 import java.util.ArrayList;
+
 //ссылочное поле list закомментировано, чтобы показать как работает клон с ссылками
 public class ObjectCloneTest {
     public static void main(String[] args) throws CloneNotSupportedException {
@@ -11,7 +12,7 @@ public class ObjectCloneTest {
         System.out.println("сразу после клоирования" + "\n" + original + "\n" + clone);
         original.primitive = 15;
         original.textclone = "By clone";
-        original.integer=115;
+        original.integer = 115;
         original.list.add(999);
         System.out.println("Изменения оригинала" + "\n" + original + "\n" + clone);
 
@@ -19,29 +20,3 @@ public class ObjectCloneTest {
     }
 }
 
-class TestClass implements Cloneable {
-    public int primitive;
-    public String textclone;
-    public Integer integer;
-    public ArrayList<Integer> list;
-
-    public TestClass(int primitive, String textclone, Integer object, ArrayList<Integer> list) {
-        this.primitive = primitive;
-        this.textclone = textclone;
-        this.list = list;
-        this.integer = object;
-    }
-
-    public TestClass clone() throws CloneNotSupportedException {
-        TestClass resalt = (TestClass) super.clone();
-//        resalt.list = new ArrayList<>();
-//        resalt.list.addAll(list);
-        return resalt;
-    }
-
-    @Override
-    public String toString() {
-        String resalt = primitive + textclone + integer + list.toString();
-        return resalt;
-    }
-}
